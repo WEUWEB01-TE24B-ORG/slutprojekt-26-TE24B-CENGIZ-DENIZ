934316a402dd60b93aa7763cef2810e1b8e143ec
TE24B-CENGIZ-DENIZ <deniz.cengiz@elev.ga.ntig.se>
2026-05-10 12:52:43 +0200

    done 100

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-// REMEMBER THE LAST SCREENSHOT
+ 
+ let angle= 0
+ document.querySelector(".video-lada").style.display = "none"
+
+function funnybutton1() {
+  alert("hi this is what it does walcome to monkey business")
+}
+
+function funnybutton2() {
+  let bruh = 10 + 11
+  alert("if 9+10 is " + bruh + " " + "then what is 40+20 ??")
+}
+
+
+function funnybutton3() {
+  document.querySelector(".video-lada").style.display = "block"
+}
+
+
+
+function funnybutton4() {
+  angle += 90
+  document.querySelector("img").style.transform = "rotate("+ angle + "deg)"
+
+}
+
+function funnybutton5() {
+ 
+ document.querySelector("main").style.backgroundColor= "brown"
+ alert("you have now ben forgiven now stop lookiong.")
+}
